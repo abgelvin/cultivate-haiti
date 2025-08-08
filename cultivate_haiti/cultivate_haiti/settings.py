28cc72ec6f5e495e0f01acb1fb4539e139e92e5c
abgelvin <60162973+abgelvin@users.noreply.github.com>
2025-08-08 15:54:48 -0600

    fixed typo

--- cultivate_haiti/cultivate_haiti/settings.py
+++ cultivate_haiti/cultivate_haiti/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_ROOT = '/home/ex2-user/cultivate-haiti/cultivate_haiti/staticfiles'
+STATIC_ROOT = '/home/ec2-user/cultivate-haiti/cultivate_haiti/staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
